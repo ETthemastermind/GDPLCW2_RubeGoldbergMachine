1771bce346ceed5a8ae89a866eea99a11aa13b78
Ethan <s4911672@bournemouth.ac.uk>
2018-05-13 21:19:49 +0100

    fixed the camera movement after I broke it and couldnt figure out how to revert to a previous version using github

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovment : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
 
-    public Transform target;
+{
+    public Transform Ball1;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = Ball1.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-
-        transform.LookAt(target);
     }
+
 }
