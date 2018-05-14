0f29852163a6e682345452d906dc147a3b2ed002
Ethan <s4911672@bournemouth.ac.uk>
2018-05-14 14:21:27 +0100

    removed a redundant script and made the hammer stationairy until the ball enters it's trigger zone

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -9,21 +9,41 @@ public class Hammer : MonoBehaviour
     public Vector3 rotateCenterOffset;
     public Vector3 rotateAxis;
     private Vector3 originalPos;
+    private Vector3 rotateCenter;
+    private bool HammerActivate = false;
 
     
     
+
+
+
     // Use this for initialization
 	void Start ()
     {
         originalPos = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("in the muller zone");
+        HammerActivate = true;
+        
+    }
+    void Update ()
     {
 
-        Vector3 rotateCenter = originalPos + rotateCenterOffset;
+        if (HammerActivate == true)
+        {
+            Debug.Log("It's Clobberin' Time!");
+            Vector3 rotateCenter = originalPos + rotateCenterOffset;
 
-        transform.RotateAround(rotateCenter, rotateAxis, 1.0f);
-	}
+            transform.RotateAround(rotateCenter, rotateAxis, speed * Time.deltaTime);
+        }
+
+
+
+
+    }
 }
