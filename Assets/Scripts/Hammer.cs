0a205d63a9fa6dea7186ee78b045ed00d33f70a5
Ethan <s4911672@bournemouth.ac.uk>
2018-05-14 14:01:54 +0100

    added second camera to follow the next sphere, added hammer and hammer script to swing it around the bottom of the hilt

---
+++ Assets/Scripts/Hammer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+
+    public float speed = 200f;
+    public Vector3 rotateCenterOffset;
+    public Vector3 rotateAxis;
+    private Vector3 originalPos;
+
+    
+    
+    // Use this for initialization
+	void Start ()
+    {
+        originalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Vector3 rotateCenter = originalPos + rotateCenterOffset;
+
+        transform.RotateAround(rotateCenter, rotateAxis, 1.0f);
+	}
+}
