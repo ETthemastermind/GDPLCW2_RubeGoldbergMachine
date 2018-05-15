28eea46e7905aa75bf1616bf877f328307d4ccf9
Ethan <s4911672@bournemouth.ac.uk>
2018-05-15 13:22:46 +0100

    added a script to add a force to the ball when it's hit by the hammer in order to send it around the loop

---
+++ Assets/HammerHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerHit : MonoBehaviour {
+    public float speed = 0f;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Hammer")
+        {
+            //Debug.Log("FORRRRRR!");
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * speed);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
