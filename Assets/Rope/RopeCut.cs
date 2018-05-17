036b51cd8d38d4bf98d949604b970b601453d512
Ethan <s4911672@bournemouth.ac.uk>
2018-05-17 16:53:58 +0100

    added the final boot device that drops when the rope is cut

---
+++ Assets/Rope/RopeCut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeCut : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "knife")
+        {
+            Debug.Log("slicey boy");
+            rb.isKinematic = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
