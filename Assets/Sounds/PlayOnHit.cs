30cf91b5608d26d6a5830c08459b096e50403c0b
Ethan <s4911672@bournemouth.ac.uk>
2018-05-18 10:31:44 +0100

    begining to add sound effects, created a script to ball a specfied sound when the object is collided with

---
+++ Assets/Sounds/PlayOnHit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnHit : MonoBehaviour {
+
+    private bool collided = false;
+    private AudioSource source;
+    [SerializeField] private AudioClip HitSound;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter()
+    {
+        if (collided == false)
+        {
+            Debug.Log("Play Sounds");
+            source.PlayOneShot(HitSound, 1f);
+            collided = true;
+            StartCoroutine("ExitCol");
+        }
+    }
+
+    IEnumerator ExitCol()
+    {
+        yield return new WaitForSeconds(0.5f);
+        collided = false;
+    }
+   
+}
