cfb87a4486d72b747649033d50ca58e75797ddc9
Ethan <s4911672@bournemouth.ac.uk>
2018-05-18 13:27:18 +0100

    added script so bullet makes noise on spawn, added a case statement so that the process of playing sounds, added more sounds

---
+++ Assets/Sounds/AudioOnSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnSpawn : MonoBehaviour {
+    public AudioSource source;
+    [SerializeField] private AudioClip HitSound;
+    [SerializeField] private AudioClip HitSound2;
+
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        source.PlayOneShot(HitSound, 1f);
+       
+    }
+
+    
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Sounds/PlayOnHit.cs
+++ Assets/Sounds/PlayOnHit.cs
@@ -5,28 +5,84 @@ using UnityEngine;
 public class PlayOnHit : MonoBehaviour {
 
     private bool collided = false;
+    private int caseSwitch = 1;
+    private bool Ball2Collided1 = false;
+    private bool StopScream = false;
     private AudioSource source;
+    public bool BallFlingException;
     [SerializeField] private AudioClip HitSound;
+    [SerializeField] private AudioClip HitSound2;
 
     private void Awake()
     {
         source = GetComponent<AudioSource>();
     }
 
-    private void OnCollisionEnter()
+    private void OnCollisionEnter(Collision other)
     {
-        if (collided == false)
+        
+        if (collided == false && BallFlingException == true)
+
+        {
+            
+            switch (caseSwitch)
+            {
+                case 1:
+                    Debug.Log("Ball Fling Exception");
+                    source.PlayOneShot(HitSound, 1f);
+                    caseSwitch = 2;
+                    
+
+                    break;
+
+                case 2:
+                    if (other.gameObject.tag != "CannonFloor")
+                    {
+                        Debug.Log("sound 2");
+                        Debug.Log("Ball Fling Exception");
+                        source.PlayOneShot(HitSound2, 1f);
+
+                    }
+                    break;
+
+                case 3:
+
+                    break;
+
+                default:
+                    Debug.Log("Stop scream error");
+                    break;
+            }
+
+            if (other.gameObject.name.Contains("Stop"))
+            {
+                caseSwitch = 3;
+            }
+            
+
+            
+
+            
+            
+            
+                
+        }
+
+        else if (collided == false)
         {
             Debug.Log("Play Sounds");
             source.PlayOneShot(HitSound, 1f);
             collided = true;
             StartCoroutine("ExitCol");
         }
+
+
+
     }
 
     IEnumerator ExitCol()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(2f);
         collided = false;
     }
    
